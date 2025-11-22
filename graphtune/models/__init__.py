746677ff978a973dfd57e7643bb584601055708a
gurwosh <64075561+gurwosh@users.noreply.github.com>
2025-11-22 16:46:37 +0900

    up5

--- graphtune/models/__init__.py
+++ graphtune/models/__init__.py
@@ -1,12 +1,10 @@
 # graphtune/models/__init__.py
-from .registry import build_model, register_model  # noqa: F401
+from .registry import build_model, register_model  # noqa
 
-# 모델 클래스 export (선택 사항)
-from .baselines import BaselineModel, HyperModel, DCRNNModel, DGCRNModel  # noqa: F401
-from .bigst import BigST  # noqa: F401
+from .baselines import BaselineModel, HyperModel, DCRNNModel, DGCRNModel  # noqa
+from .bigst.model import BigST  # ✅ 이렇게 명시하면 bigst.py 파일이 있어도 안 꼬임
 
-# 등록(side-effect)
-from . import factories  # noqa: F401
+from . import factories  # noqa
 
 __all__ = [
     "build_model", "register_model",
---
+++ graphtune/models/bigst/__init__.py
@@ -0,0 +1,4 @@
+# graphtune/models/bigst/__init__.py
+from .model import BigST
+
+__all__ = ["BigST"]
