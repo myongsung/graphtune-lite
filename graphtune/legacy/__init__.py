b31f822706ac8fe60e78f20c2d082ecdd23dc4e4
gurwosh <64075561+gurwosh@users.noreply.github.com>
2025-11-22 16:38:13 +0900

    up5

--- graphtune/legacy/__init__.py
+++ graphtune/legacy/__init__.py
@@ -1,4 +1,16 @@
-from .data import prepare_dataset
-from .models import build_model
-from .train import train_one_stage
-from .utils import load_partial_state
+"""
+Legacy namespace (v2 migration).
+
+Do NOT import models here anymore.
+Models are provided from graphtune.models (v2).
+"""
+
+from .data import prepare_dataset  # noqa: F401
+from .train import train_one_stage  # noqa: F401
+from .utils import load_partial_state  # noqa: F401
+
+__all__ = [
+    "prepare_dataset",
+    "train_one_stage",
+    "load_partial_state",
+]
