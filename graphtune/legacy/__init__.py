34236db696dcae0a2b6e5e5a582844dba4b8cab9
gurwosh <64075561+gurwosh@users.noreply.github.com>
2025-11-22 19:45:34 +0900

    up6

--- graphtune/legacy/__init__.py
+++ graphtune/legacy/__init__.py
@@ -1,14 +1,7 @@
 """
 Legacy namespace (v2 migration).
 
-Legacy is now only for old code that hasn't been moved yet.
-Do NOT import legacy.data here anymore.
+Do not auto-import anything here.
+Import legacy submodules explicitly where needed.
 """
-
-from .train import train_one_stage  # noqa: F401
-from .utils import load_partial_state  # noqa: F401
-
-__all__ = [
-    "train_one_stage",
-    "load_partial_state",
-]
+__all__ = []
